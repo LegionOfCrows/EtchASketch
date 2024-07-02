e7558280c0bdb887f399fc264a20d1d8580cc683
Marsel <reptileeeeee@gmail.com>
2024-07-02 11:53:15 +0300

    addEventListenerLoaded

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+let body = document.body;
+
 let sideLength = 16;
 
 for (let index = 0; index <sideLength; index++) {
     for (let j = 0; index <sideLength; j++){
         let drawCell = document.createElement("div");
         drawCell.classList.add("drawCell");
-        document.body.appendChild(drawCell)
+        body.appendChild(drawCell)
 
     }
     
-}
+}})
