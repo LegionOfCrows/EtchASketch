92e109b587c112a69091b285b6c38dae3921a1be
Marsel <reptileeeeee@gmail.com>
2024-07-02 11:48:09 +0300

    createDiv

---
+++ script.js
@@ -0,0 +1,11 @@
+let sideLength = 16;
+
+for (let index = 0; index <sideLength; index++) {
+    for (let j = 0; index <sideLength; j++){
+        let drawCell = document.createElement("div");
+        drawCell.classList.add("drawCell");
+        document.body.appendChild(drawCell)
+
+    }
+    
+}
