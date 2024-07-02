f31b13b78409243bcadb930f676cda1f6ec5dbc0
Marsel <reptileeeeee@gmail.com>
2024-07-02 16:14:00 +0300

    comleted grid

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ createGrid(sideLength);
 })
 
 function createGrid(sideLength){
+const containerWidth = document.getElementById('container').clientWidth
+console.log(containerWidth)
+const cellSize = (containerWidth/sideLength) - 2;
 for (let index = 0; index <sideLength; index++) {
     for (let j = 0; j <sideLength; j++){
         let drawCell = document.createElement("div");
         drawCell.classList.add("drawCell");
+        drawCell.style.width = `${cellSize}px`;
+        drawCell.style.height = `${cellSize}px`;
         container.appendChild(drawCell);
 
     }
