9f79e9c09233e88dcb6b48a6711081dc81d3a423
Marsel <reptileeeeee@gmail.com>
2024-07-19 16:18:45 +0300

    made basic drawing

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ createGrid(sideLength);
 })
 
 function createGrid(sideLength){
-const containerWidth = document.getElementById('container').clientWidth
-console.log(containerWidth)
-const cellSize = (containerWidth/sideLength) - 2;
+// const containerWidth = document.getElementById('container').clientWidth
+// console.log(containerWidth)
+const cellSize = (960/sideLength) - 2.65;
 for (let index = 0; index <sideLength; index++) {
     for (let j = 0; j <sideLength; j++){
         let drawCell = document.createElement("div");
         drawCell.classList.add("drawCell");
         drawCell.style.width = `${cellSize}px`;
         drawCell.style.height = `${cellSize}px`;
+        drawCell.addEventListener("mouseover", () => changeColor(drawCell));
         container.appendChild(drawCell);
 
-    }
-    
+    }    
 }}
+function changeColor(cell){
+    cell.style.backgroundColor = "green";
+}
