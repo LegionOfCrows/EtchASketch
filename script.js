e462efbabc429e306fe8d04d96ef740b7bde63e0
LegionOfCrows <reptileeeeee@gmail.com>
2024-07-26 00:39:05 +0300

    add size changing

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    
+const sizeButton = document.querySelector("#size");
+sizeButton.addEventListener("click", () => {
+    sideLength = prompt("Размер сетки:",[16]);
+    if (sideLength > 100){
+        alert("Слишком большое число");
+    } else {
+        removeGrid();
+        createGrid(sideLength);
+    }
+})
 const container = document.getElementById('container');
 let sideLength = 16
 createGrid(sideLength);
@@ -8,7 +18,7 @@ createGrid(sideLength);
 function createGrid(sideLength){
 // const containerWidth = document.getElementById('container').clientWidth
 // console.log(containerWidth)
-const cellSize = (960/sideLength) - 2.65;
+const cellSize = (960/sideLength) - 2;
 for (let index = 0; index <sideLength; index++) {
     for (let j = 0; j <sideLength; j++){
         let drawCell = document.createElement("div");
@@ -22,4 +32,9 @@ for (let index = 0; index <sideLength; index++) {
 }}
 function changeColor(cell){
     cell.style.backgroundColor = "green";
+}
+function removeGrid(){
+    while (container.firstChild){
+        container.removeChild(container.lastChild)
+    }
 }
